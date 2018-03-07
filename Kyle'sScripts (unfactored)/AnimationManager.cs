a5319133d81d965a5b0b74911b84ec2338a43fb3
Kyle_Pinkham <kyledpinkham@Gmail.com>
2018-03-06 21:54:50 -0700

    Added Maya files and Unity Scripts for Class
    Added Maya files and Unity Scripts for Class

---
+++ Kyle'sScripts (unfactored)/AnimationManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AnimationManager : MonoBehaviour {
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Walking()
+    {
+        animator.SetFloat("isWalking", 1);
+    }
+
+    public void Magic()
+    {
+        animator.SetTrigger("castMagic");
+    }
+
+    public void Attack()
+    {
+        animator.SetTrigger("useAttack");
+    }
+
+}
---
+++ Kyle'sScripts (unfactored)/HueJump.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HueJump : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.H)){
+            CharacterController cc = this.GetComponent<CharacterController>();
+            cc.transform.Translate(0, 4, 0);
+
+        }
+	}
+}
